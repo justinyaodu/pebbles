46214d623fd600b42150a04cac0612ea9ee55e52
Melody Ruth <maruth@ucsd.edu>
2023-02-10 12:34:17 -0800

    get max depth, flip depths to be weights

--- synth/parser.cpp
+++ synth/parser.cpp
@@ -7,6 +7,12 @@ using namespace std;
 
 int main()
 {
+    //What we're getting from the input file (todo: create a spec from this)
+    uint32_t numVariables = 0;
+    uint32_t maxDepth = 0;//e.g. this will be 4 for the D5 files since the grammar has "Start" as a sort of 0 level depth
+    vector<uint32_t> var_depths;//depths range from 0 to maxDepth, represent the "weight" of the variable in the tree
+    vector<string> var_names;
+
     //open SyGuS-formatted input file
     ifstream inputFile;
     inputFile.open("input.sl");
@@ -15,8 +21,6 @@ int main()
     bool finishedGrammar = false;
     int depth = 0;
     string varName;
-    vector<uint32_t> var_depths;
-    vector<string> var_names;
     while (getline(inputFile, line))
     {
         if (lineNumber > 10 && !finishedGrammar)
@@ -37,10 +41,19 @@ int main()
         }
         lineNumber++;
     }
+    maxDepth = depth;
+    numVariables = var_names.size();
     inputFile.close();
 
+    //Flip depths to be "weight"s instead
+    for (int i = 0; i < numVariables; i++)
+    {
+        var_depths[i] = maxDepth - var_depths[i];
+    }
+
     //print out all the variables
-    for (int i = 0; i < var_names.size(); i++) {
+    for (int i = 0; i < numVariables; i++)
+    {
         cout << var_names[i] << ": depth " << var_depths[i] << endl;
     }
 }
