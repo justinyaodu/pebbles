56817087585953870fe36ec457eb4214c43b7d30
Justin Yao Du <justinyaodu@gmail.com>
2023-02-11 09:54:53 -0800

    Replace tabs with 4 spaces

--- synth/expr.hpp
+++ synth/expr.hpp
@@ -7,92 +7,75 @@
 
 class Expr {
 private:
-	static const int32_t AND = -1;
-	static const int32_t OR = -2;
-	static const int32_t XOR = -3;
-	static const int32_t NOT = -4;
+    static const int32_t AND = -1;
+    static const int32_t OR = -2;
+    static const int32_t XOR = -3;
+    static const int32_t NOT = -4;
 
-	// AND, OR, XOR, NOT, or the number of a variable (e.g. 2 for x2).
-	int32_t type;
+    // AND, OR, XOR, NOT, or the number of a variable (e.g. 2 for x2).
+    int32_t type;
 
-	// Left and right children, if present.
-	Expr* left;
-	Expr* right;
+    // Left and right children, if present.
+    Expr* left;
+    Expr* right;
 
-	Expr(int32_t type, Expr* left, Expr* right) :
-		type(type), left(left), right(right) {}
+    Expr(int32_t type, Expr* left, Expr* right) :
+        type(type), left(left), right(right) {}
 
 public:
-	// Static helpers to construct various expressions.
+    // Static helpers to construct various expressions.
 
-	static Expr And(Expr *left, Expr *right) {
-		return Expr(Expr::AND, left, right);
-	}
+    static Expr And(Expr *left, Expr *right) {
+        return Expr(Expr::AND, left, right);
+    }
 
-	static Expr Or(Expr *left, Expr *right) {
-		return Expr(Expr::OR, left, right);
-	}
+    static Expr Or(Expr *left, Expr *right) {
+        return Expr(Expr::OR, left, right);
+    }
 
-	static Expr Xor(Expr *left, Expr *right) {
-		return Expr(Expr::XOR, left, right);
-	}
+    static Expr Xor(Expr *left, Expr *right) {
+        return Expr(Expr::XOR, left, right);
+    }
 
-	static Expr Not(Expr *left) {
-		return Expr(Expr::NOT, left, nullptr);
-	}
+    static Expr Not(Expr *left) {
+        return Expr(Expr::NOT, left, nullptr);
+    }
 
-	static Expr Var(int32_t var_num) {
-		return Expr(var_num, nullptr, nullptr);
-	}
+    static Expr Var(int32_t var_num) {
+        return Expr(var_num, nullptr, nullptr);
+    }
 
-	friend std::ostream& operator<< (std::ostream &out, const Expr &expr) {
-		switch (expr.type) {
-			case Expr::AND:
-				return out
-					<< "("
-					<< *(expr.left)
-					<< " && "
-					<< *(expr.right)
-					<< ")";
-			case Expr::OR:
-				return out
-					<< "("
-					<< *(expr.left)
-					<< " || "
-					<< *(expr.right)
-					<< ")";
-			case Expr::XOR:
-				return out
-					<< "("
-					<< *(expr.left)
-					<< " ^ "
-					<< *(expr.right)
-					<< ")";
-			case Expr::NOT:
-				return out
-					<< "!"
-					<< *(expr.left);
-			default:
-				return out << "x" << expr.type;
-		}
-	}
+    friend std::ostream& operator<< (std::ostream &out, const Expr &expr) {
+        switch (expr.type) {
+            case Expr::AND:
+                return out << "(" << *(expr.left) << " && " << *(expr.right) << ")";
+            case Expr::OR:
+                return out << "(" << *(expr.left) << " || " << *(expr.right) << ")";
+            case Expr::XOR:
+                return out << "(" << *(expr.left) << " ^ " << *(expr.right) << ")";
+            case Expr::NOT:
+                return out << "!" << *(expr.left);
+            default:
+                return out << "x" << expr.type;
+        }
+    }
 
-	// Evaluate an expression with the given variable values.
-	// The i'th element of vars is the value of the i'th variable.
-	bool eval(const std::vector<bool> &vars) {
-		switch (this->type) {
-			case Expr::AND:
-				return this->left->eval(vars) && this->right->eval(vars);
-			case Expr::OR:
-				return this->left->eval(vars) || this->right->eval(vars);
-			case Expr::XOR:
-				return this->left->eval(vars) ^ this->right->eval(vars);
-			case Expr::NOT:
-				return !this->left->eval(vars);
-			default:
-				return vars[this->type];
-		}
-	}
+    // Evaluate an expression with the given variable values.
+    // The i'th element of vars is the value of the i'th variable.
+    bool eval(const std::vector<bool> &vars) {
+        switch (this->type) {
+            case Expr::AND:
+                return this->left->eval(vars) && this->right->eval(vars);
+            case Expr::OR:
+                return this->left->eval(vars) || this->right->eval(vars);
+            case Expr::XOR:
+                return this->left->eval(vars) ^ this->right->eval(vars);
+            case Expr::NOT:
+                return !this->left->eval(vars);
+            default:
+                return vars[this->type];
+        }
+    }
 };
 
 #endif
--- synth/reference.cpp
+++ synth/reference.cpp
@@ -8,63 +8,71 @@
 
 class Term {
 public:
-	// The i'th bit is the result of evaluating this term on example j.
-	uint32_t result;
+    // The i'th bit is the result of evaluating this term on example i.
+    uint32_t result;
 
-	// The index of this term's left child in the preceding bank.
-	uint32_t left;
+    // The index of this term's left child in the preceding bank.
+    uint32_t left;
 
-	// The index of this term's right child in the preceding bank.
-	uint32_t right;
+    // The index of this term's right child in the preceding bank.
+    uint32_t right;
 
-	Term(uint32_t result, uint32_t left, uint32_t right) :
-		result(result), left(left), right(right) {}
+    Term(uint32_t result, uint32_t left, uint32_t right) :
+        result(result), left(left), right(right) {}
 };
 
 class Bank {
 private:
-	// The i'th bit is on iff the bank contains a term whose bitvector
-	// of evaluation results is equal to i.
-	std::bitset<1L << 32> bitset;
+    // The i'th bit is on iff the bank contains a term whose bitvector
+    // of evaluation results is equal to i.
+    std::bitset<1L << 32> bitset;
 
-	std::vector<Term> terms;
+    std::vector<Term> terms;
+
+    // End indices of each section of terms (variables, NOTs, ANDs, ORs, XORs).
+    std::vector<size_t> section_boundaries;
 
 public:
-	// Add a term to the bank, unless another term evaluating to the same
-	// value has been seen before.
-	bool insert(Term &term) {
-		if (this->bitset[term.result]) {
-			return false;
-		}
-		this->bitset[term.result] = true;
-		this->terms.push_back(term);
-		return true;
-	}
+    // Add a term to the bank, unless another term evaluating to the same
+    // value has been seen before.
+    bool insert(Term &term) {
+        if (this->bitset[term.result]) {
+            return false;
+        }
+        this->bitset[term.result] = true;
+        this->terms.push_back(term);
+        return true;
+    }
+
+    // Indicate the end of a section of terms.
+    void end_section() {
+        this->section_boundaries.push_back(this->terms.size());
+    }
 };
 
 class Synthesizer {
-	Spec &spec;
+    Spec &spec;
 
-	// Banks of terms for each depth.
-	std::vector<Bank> banks;
+    // Banks of terms for each depth.
+    std::vector<Bank> banks;
 
-	Synthesizer(Spec spec) : spec(spec) {}
+    Synthesizer(Spec spec) : spec(spec) {}
 };
 
 int main(void) {
-	// No synthesizer yet - testing boolean expressions.
+    // No synthesizer yet - testing boolean expressions.
 
-	Expr x0 = Expr::Var(0);
-	Expr x1 = Expr::Var(1);
-	Expr x2 = Expr::Var(2);
-	Expr x3 = Expr::Var(3);
-	Expr not_zero = Expr::Not(&x0);
-	Expr not_zero_xor_one = Expr::Xor(&not_zero, &x1);
-	Expr two_and_three = Expr::And(&x2, &x3);
-	Expr top = Expr::Or(&not_zero_xor_one, &two_and_three);
-	std::cout << top << std::endl;
+    Expr x0 = Expr::Var(0);
+    Expr x1 = Expr::Var(1);
+    Expr x2 = Expr::Var(2);
+    Expr x3 = Expr::Var(3);
+    Expr not_zero = Expr::Not(&x0);
+    Expr not_zero_xor_one = Expr::Xor(&not_zero, &x1);
+    Expr two_and_three = Expr::And(&x2, &x3);
+    Expr top = Expr::Or(&not_zero_xor_one, &two_and_three);
+    std::cout << top << std::endl;
 
-	// x0 = false, x1 = false, x2 = true, x3 = false
-	std::vector<bool> var_values { false, false, true, false };
-	std::cout << top.eval(var_values) << std::endl;
+    // x0 = false, x1 = false, x2 = true, x3 = false
+    std::vector<bool> var_values { false, false, true, false };
+    std::cout << top.eval(var_values) << std::endl;
 }
