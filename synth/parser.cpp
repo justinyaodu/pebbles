5fdd8daeb93456cd807c6e1a561c78450feeaa71
Melody Ruth <maruth@ucsd.edu>
2023-03-05 19:53:37 -0800

    switching custom input parser to the new spec

--- synth/parser.cpp
+++ synth/parser.cpp
@@ -185,7 +185,6 @@ Spec Parser::parseTruthTableInput(string inputFileName) {
     int32_t maxHeight = 0;//e.g. this will be 4 for the D5 files since the grammar has "Start" as a sort of 0 level height
     vector<int32_t> var_heights;//heights range from 0 to maxHeight, represent the "weight" of the variable in the tree
     vector<string> var_names;
-    uint32_t sol_result;
     uint32_t num_examples;
     vector<vector<bool>> all_inputs;
     vector<bool> full_sol;
@@ -247,19 +246,10 @@ Spec Parser::parseTruthTableInput(string inputFileName) {
 	    cout << "output: " << full_sol[i] << endl;
     }*/
 
-    // restricted input/output
-    vector<uint32_t> vals;
-    for (uint32_t i = 0; i < numVariables; i++) {
-        vals.push_back(0);
-    }
-    sol_result = truthTableWithVecFromTruthTable(full_sol, all_inputs, vals);
-
     return Spec(numVariables, 
                 num_examples, 
                 var_names, 
-                var_heights, 
-                vals, 
-                sol_result, 
+                var_heights,
                 maxHeight,
                 all_inputs,
                 full_sol);
@@ -349,6 +339,7 @@ Spec Parser::parseInput(string inputFileName) {
 
     if (numVariables > 31) {
         // we can't even parse this many. abort
+        cout << "Abandoning this spec because it has too many (" << numVariables << ") variables" << endl;
         return Spec(numVariables, 
                 0, 
                 var_names, 
@@ -368,7 +359,7 @@ Spec Parser::parseInput(string inputFileName) {
     cout<<"spec made"<<std::endl;
 
     return Spec(numVariables, 
-                1, 
+                num_examples, 
                 var_names, 
                 var_depths,
                 maxDepth,
