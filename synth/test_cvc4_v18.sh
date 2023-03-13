e3e42767e670ff3ca0424dcd7c118c223b23a25e
Melody Ruth <maruth@ucsd.edu>
2023-03-12 21:31:36 -0700

    cleaning up script

--- synth/test_cvc4_v18.sh
+++ synth/test_cvc4_v18.sh
@@ -4,5 +4,5 @@ FILES="./cvc4_test_inputs/*"
 for file in $FILES
 do
     echo "$file"
-    time timeout 300s ./cvc4-1.7-x86_64-linux-opt $file
+    time timeout 300s ./cvc4-1.7-x86_64-linux-opt $file --lang=sygus1 $file
 done
