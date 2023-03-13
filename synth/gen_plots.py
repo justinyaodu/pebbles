366887659c789d9c0d0fab339211feddd8856763
Melody Ruth <maruth@ucsd.edu>
2023-03-13 14:54:57 -0700

    better plot

--- synth/gen_plots.py
+++ synth/gen_plots.py
@@ -6,14 +6,19 @@ with open('comparison_cvc4_pebbles.txt') as f:
     cvc4_times = [float(line.split()[0]) for line in lines]
     pebbles_times = [float(line.split()[1]) for line in lines]
 
-#cvc4_times = [min(5,y) for y in cvc4_times]
-
-print(cvc4_times)
-#print(pebbles_times)
+# Instead of directly sorting, get an array of what the indices should be to sort cvc4_times
+# Then we sort both cvc4_times and pebbbles_times according to that order
+sort_order = np.argsort(cvc4_times)
+cvc4_times = np.array(cvc4_times)[sort_order]
+pebbles_times = np.array(pebbles_times)[sort_order]
 
 plt.yscale('log')
-plt.plot(range(len(cvc4_times)),cvc4_times,'ro--',linewidth=0.5, markersize=1.5, label="cvc4")
-plt.plot(range(len(cvc4_times)),pebbles_times,'go--',linewidth=0.5, markersize=1.5, label="pebbles")
+plt.plot(cvc4_times,'ro--',linewidth=0.5, markersize=1.5, label="cvc4")
+plt.plot(pebbles_times,'go--',linewidth=0.5, markersize=1.5, label="pebbles")
+plt.ylabel("Execution time in seconds")
+plt.xlabel("SyGuS CrCi problems")
+plt.title("Log-scale comparison of CVC4 1.8 and Pebbles")
+plt.xticks([], []) # turn off x-ticks
 leg = plt.legend(loc='upper left')
-plt.savefig('cvc4_versus_pebbles2.png')
+plt.savefig('cvc4_versus_pebbles.png')
 plt.show()
