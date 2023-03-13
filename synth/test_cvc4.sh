31b415ceff3f4e6de859793528fa939c4ffa1d51
Melody Ruth <melodyaruth@yahoo.com>
2023-03-12 21:18:39 -0700

    ran on dsmlp

--- synth/test_cvc4.sh
+++ synth/test_cvc4.sh
@@ -4,9 +4,9 @@ FILES="./cvc4_test_inputs/*"
 for file in $FILES
 do
     echo "$file"
-    start_time="$(date -u +%s.%N)"
-    ./cvc4-1.7-win64-opt.exe $file
-    end_time="$(date -u +%s.%N)"
-    elapsed=$(echo "scale=3; $end_time - $start_time" | bc)
-    echo -e "$elapsed seconds\n"
+    #start_time="$(date -u +%s.%N)"
+    time timeout 300s ./cvc4-1.7-x86_64-linux-opt $file
+    #end_time="$(date -u +%s.%N)"
+    #elapsed=$(echo "scale=3; $end_time - $start_time" | bc)
+    #echo -e "$elapsed seconds\n"
 done
