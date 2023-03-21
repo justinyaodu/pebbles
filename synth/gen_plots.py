c9451fe9645464962b4d951b7d36f36c2384940d
Melody Ruth <maruth@ucsd.edu>
2023-03-20 21:46:18 -0700

    updating graphs

--- synth/gen_plots.py
+++ synth/gen_plots.py
@@ -22,15 +22,15 @@ with open('found.txt') as f:
 
 with open('singlethread10.txt') as f:
     lines = f.readlines()
-    pebbles_times_st_random10 = [sum([float(line) for line in lines])] * 16
+    pebbles_times_st_random10 = [sum([float(line) for line in lines])] * 8
 
 with open('multithread10.txt') as f:
     lines = f.readlines()
-    pebbles_times_mt_random10 = [sum([float(line) for line in lines])] * 16
+    pebbles_times_mt_random10 = [sum([float(line) for line in lines])] * 8
 
 with open('gpu_version10.txt') as f:
     lines = f.readlines()
-    pebbles_times_gpu_random10 = [sum([float(line) for line in lines if not line.startswith("Synthesizer")])] * 16
+    pebbles_times_gpu_random10 = [sum([float(line) for line in lines if not line.startswith("Synthesizer")])] * 8
 
 with open('singlethread100.txt') as f:
     lines = f.readlines()
@@ -48,7 +48,7 @@ with open('compareNumThreads10_2.txt') as f:
     lines = f.readlines()
     compare_thread_count_times = [float(line) for line in lines]
 threadTimes = []
-for i in range(16):
+for i in range(8):
     threadTimes.append(sum(compare_thread_count_times[i*10:(i+1)*10]))
 
 # Instead of directly sorting, get an array of what the indices should be to sort cvc4_times
@@ -70,61 +70,85 @@ pebbles_times_solved = pebbles_times[has_solution]
 cvc4_times_no_solution = cvc4_times[np.invert(has_solution)]
 pebbles_times_no_solution = pebbles_times[np.invert(has_solution)]
 
-plt.yscale('log')
-plt.plot(cvc4_times_solved,'o',markersize=2, label="cvc4")
-plt.plot(pebbles_times_solved,'o',markersize=2, label="pebbles")
-plt.ylabel("Execution time in seconds")
-plt.xlabel("SyGuS CrCi problems with solutions")
-plt.title("Log-scale comparison of CVC4 1.8 and Pebbles")
-plt.xticks([], []) # turn off x-ticks
-leg = plt.legend(loc='upper left')
-plt.savefig('cvc4_versus_pebbles_solved.png')
-plt.show()
-
-plt.yscale('log')
-plt.plot(cvc4_times_no_solution,'o',markersize=2, label="cvc4")
-plt.plot(pebbles_times_no_solution,'o',markersize=2, label="pebbles")
-plt.ylabel("Execution time in seconds")
-plt.xlabel("SyGuS CrCi problems with no solution")
-plt.title("Log-scale comparison of CVC4 1.8 and Pebbles")
-plt.xticks([], []) # turn off x-ticks
-leg = plt.legend(loc='upper left')
-plt.savefig('cvc4_versus_pebbles_unsolved.png')
-plt.show()
-
-plt.yscale('log')
-plt.plot(pebbles_times,'o',markersize=2, label="singlethread")
-plt.plot(pebbles_times_mt,'o',markersize=2, label="multithread")
-plt.plot(pebbles_times_gpu,'o', markersize=2, label="gpu")
+ax = plt.gca()
+ax.set_ylim([0, 10])
+plt.plot(cvc4_times,'o',markersize=2, label="cvc4")
+plt.plot(pebbles_times,'o',markersize=2, label="pebbles")
 plt.ylabel("Execution time in seconds")
 plt.xlabel("SyGuS CrCi problems")
-plt.title("Comparison of Pebbles with single thread versus multithread")
+plt.title("Comparison of CVC4 1.8 and Pebbles")
 plt.xticks([], []) # turn off x-ticks
 leg = plt.legend(loc='upper left')
-plt.savefig('single_versus_multi.png')
+plt.savefig('cvc4_versus_pebbles_linear.png')
 plt.show()
 
 plt.yscale('log')
-plt.plot(pebbles_times_st_random,'o', markersize=2, label="singlethread")
-plt.plot(pebbles_times_mt_random,'o', markersize=2, label="multithread")
-plt.plot(pebbles_times_gpu_random,'o', markersize=2, label="gpu")
+plt.plot(cvc4_times,'o',markersize=2, label="cvc4")
+plt.plot(pebbles_times,'o',markersize=2, label="pebbles")
 plt.ylabel("Execution time in seconds")
-plt.xlabel("Random 5 variable truth tables")
-plt.title("Comparison of Pebbles with single thread versus multithread")
+plt.xlabel("SyGuS CrCi problems")
+plt.title("Log-scale comparison of CVC4 1.8 and Pebbles")
 plt.xticks([], []) # turn off x-ticks
 leg = plt.legend(loc='upper left')
-plt.savefig('single_versus_multi_random100.png')
+plt.savefig('cvc4_versus_pebbles_log.png')
 plt.show()
 
-plt.yscale('log')
-plt.plot(pebbles_times_st_random10,'o-',linewidth=1, markersize=2.5, label="singlethread")
-plt.plot(threadTimes,'o-',linewidth=1, markersize=2.5, label="Multithread varying thread count")
-plt.plot(pebbles_times_gpu_random10,'o-',linewidth=1, markersize=2.5, label="gpu")
-#plt.plot(pebbles_times_mt_random10,'o-',linewidth=1, markersize=2.5, label="multithread")
-plt.ylabel("Execution time in seconds (total all random benchmarks)")
-plt.xlabel("Thread count")
-plt.title("Comparison of multithread Pebbles with different thread counts")
-plt.xticks(range(16), range(1,17)) # turn off x-ticks
-leg = plt.legend(loc='upper left')
-plt.savefig('thread_count_comparison.png')
-plt.show()
+# plt.yscale('log')
+# plt.plot(cvc4_times_solved,'o',markersize=2, label="cvc4")
+# plt.plot(pebbles_times_solved,'o',markersize=2, label="pebbles")
+# print(pebbles_times[len(pebbles_times)//2+2],cvc4_times[len(pebbles_times)//2+2])
+# plt.ylabel("Execution time in seconds")
+# plt.xlabel("SyGuS CrCi problems with solutions")
+# plt.title("Log-scale comparison of CVC4 1.8 and Pebbles")
+# plt.xticks([], []) # turn off x-ticks
+# leg = plt.legend(loc='upper left')
+# plt.savefig('cvc4_versus_pebbles_solved.png')
+# plt.show()
+
+# plt.yscale('log')
+# plt.plot(cvc4_times_no_solution,'o',markersize=2, label="cvc4")
+# plt.plot(pebbles_times_no_solution,'o',markersize=2, label="pebbles")
+# plt.ylabel("Execution time in seconds")
+# plt.xlabel("SyGuS CrCi problems with no solution")
+# plt.title("Log-scale comparison of CVC4 1.8 and Pebbles")
+# plt.xticks([], []) # turn off x-ticks
+# leg = plt.legend(loc='upper left')
+# plt.savefig('cvc4_versus_pebbles_unsolved.png')
+# plt.show()
+
+# plt.yscale('log')
+# plt.plot(pebbles_times,'o',markersize=2, label="singlethread")
+# plt.plot(pebbles_times_mt,'o',markersize=2, label="multithread")
+# plt.plot(pebbles_times_gpu,'o', markersize=2, label="gpu")
+# plt.ylabel("Execution time in seconds")
+# plt.xlabel("SyGuS CrCi problems")
+# plt.title("Comparison of Pebbles with single thread versus multithread")
+# plt.xticks([], []) # turn off x-ticks
+# leg = plt.legend(loc='upper left')
+# plt.savefig('single_versus_multi.png')
+# plt.show()
+
+# plt.yscale('log')
+# plt.plot(pebbles_times_st_random,'o', markersize=2, label="singlethread")
+# plt.plot(pebbles_times_mt_random,'o', markersize=2, label="multithread")
+# plt.plot(pebbles_times_gpu_random,'o', markersize=2, label="gpu")
+# plt.ylabel("Execution time in seconds")
+# plt.xlabel("Random 5 variable truth tables")
+# plt.title("Comparison of Pebbles with single thread versus multithread")
+# plt.xticks([], []) # turn off x-ticks
+# leg = plt.legend(loc='upper left')
+# plt.savefig('single_versus_multi_random100.png')
+# plt.show()
+
+#plt.yscale('log')
+# plt.plot(pebbles_times_st_random10,'-',linewidth=1, markersize=2.5, label="singlethread")
+# plt.plot(threadTimes,'o-',linewidth=1, markersize=2.5, label="multithread")
+# plt.plot(pebbles_times_gpu_random10,'-',linewidth=1, markersize=2.5, label="gpu")
+# #plt.plot(pebbles_times_mt_random10,'o-',linewidth=1, markersize=2.5, label="multithread")
+# plt.ylabel("Execution time in seconds (total all random benchmarks)")
+# plt.xlabel("Thread count")
+# plt.title("Comparison of multithread Pebbles with different thread counts")
+# plt.xticks(range(8), range(1,9)) # turn off x-ticks
+# leg = plt.legend(loc='upper right')
+# plt.savefig('thread_count_comparison.png')
+# plt.show()
