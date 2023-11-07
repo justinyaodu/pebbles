7789aca33cfb1bb36f1c7421bfe1e2b8fe85e614
Melody Ruth <maruth@ucsd.edu>
2023-11-07 10:47:48 -0800

    fix script

--- synth/test_cpu_mt_compare.sh
+++ synth/test_cpu_mt_compare.sh
@@ -4,13 +4,13 @@ FILES="./random_5s_10exs/*"
 TIMEFORMAT=%R
 
 
-for (( OMP_NUM_THREADS = 1; OMP_NUM_THREADS <= 16; OMP_NUM_THREADS++ ));
+for (( NUM_THREADS = 1; NUM_THREADS <= 8; NUM_THREADS++ ));
 do
-    echo "$OMP_NUM_THREADS threads"
+    echo "$NUM_THREADS threads"
     for file in $FILES
     do
         echo "$file"
-        time_taken=$((time timeout 300s ./synth_cpu_mt_input_file $file) 2>&1 >/dev/null)
+        time_taken=$((OMP_NUM_THREADS=$NUM_THREADS time timeout 300s ./synth_cpu_mt_input_file $file) 2>&1 >/dev/null)
         echo "$time_taken" >>/dev/stderr
     done
 done
